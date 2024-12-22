0986be4583e146e8f67a58d9bb06f5007313afbf
Bambi <mayuko.hirono@gmail.com>
2024-12-22 21:40:54 +0900

    Add core app

--- charityproject/charityproject/settings.py
+++ charityproject/charityproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "core.apps.CoreConfig",
     "faqs.apps.FaqsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
