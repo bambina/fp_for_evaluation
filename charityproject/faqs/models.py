99cc9f97d87977633441d2196ef398acc1d98e70
Bambi <mayuko.hirono@gmail.com>
2024-12-23 15:44:08 +0900

    Show DB data on FAQ page

--- charityproject/faqs/models.py
+++ charityproject/faqs/models.py
@@ -14,6 +14,7 @@ class FAQEntry(models.Model):
         indexes = [
             models.Index(fields=["question", "answer"]),
         ]
+        ordering = ["created_at"]
 
     def __str__(self):
         return self.question
--- charityproject/faqs/views.py
+++ charityproject/faqs/views.py
@@ -1,6 +1,12 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from django.http import HttpResponse
+from faqs.models import *
 
 
 def faq_list(request):
-    return render(request, "faqs/list.html")
+    faqs = FAQEntry.objects.all()
+    paginator = Paginator(faqs, 4)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "faqs/list.html", {"faqs": page_obj})
