110a5f357350ee389d5b00cd3841981119034aa6
Bambi <mayuko.hirono@gmail.com>
2025-01-09 21:11:23 +0900

    Display child data on the Sponsor a Child page

--- charityproject/sponsors/views.py
+++ charityproject/sponsors/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from sponsors.models import *
 
 
 def child_list(request):
-    return render(request, "sponsors/child_list.html")
+    children = Child.objects.all()
+    paginator = Paginator(children, 4)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "sponsors/child_list.html", {"children": page_obj})
