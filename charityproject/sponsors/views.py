c0dca6b407e8cf3a937a729fb8f5aacd3aa6c2a6
Bambi <mayuko.hirono@gmail.com>
2025-01-10 09:33:44 +0900

    Organize data displayed on the Sponsor a Child page

--- charityproject/sponsors/views.py
+++ charityproject/sponsors/views.py
@@ -5,8 +5,8 @@ from sponsors.models import *
 
 
 def child_list(request):
-    children = Child.objects.all()
-    paginator = Paginator(children, 4)
+    children = Child.objects.select_related("country").all()
+    paginator = Paginator(children, 6)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(request, "sponsors/child_list.html", {"children": page_obj})
